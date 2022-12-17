c67c62f55201c652a5d51149977a3dcd85aef762
Suda94 <grg_sudarshan94@hotmail.com>
2022-12-17 23:42:03 +0000

    comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//button operations:
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -5,3 +6,4 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('data-current-operand');
+
