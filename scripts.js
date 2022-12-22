c44d2d7d6354705d381b995945ea2494d7bd353b
Suda94 <grg_sudarshan94@hotmail.com>
2022-12-22 22:12:58 +0000

    adding operation computing

--- scripts.js
+++ scripts.js
@@ -41,7 +41,25 @@ chooseOperation(operation)
 }
 
 compute()
-{
+{ met computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current))
+        return
+    switch (this.operation) {
+        case '+':
+            computation = prev + current;
+            break
+        case '-':
+            computation = prev - current;
+            break
+        case '*':
+            computation = prev * current;
+            break
+        case '/':
+            computation = prev / current
+            break
+    }
 
 
 }
