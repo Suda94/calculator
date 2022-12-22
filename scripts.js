51b0fab703e6ebe3fdf46441b0cdff1e0df81a19
Suda94 <grg_sudarshan94@hotmail.com>
2022-12-22 22:27:30 +0000

    display number function

--- scripts.js
+++ scripts.js
@@ -68,6 +68,22 @@ compute()
     this.previousOperand = ''
 }
 
+getDisplayNumber(number){
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+        integerDisplay = '';
+    } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+    }
+    if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`;
+    } else {
+        return integerDisplay;
+    }
+}
 
 
 updateDisplay()
